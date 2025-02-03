9f307c2b3e16d07a62561b8061f81e5906a83340
Sarah Cross <sarah.cross@haverford.com>
2025-02-02 22:57:17 -0500

    Added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import ise from "./assets/ise.webp";
 import tartanHacks from "./assets/tartanhacks.jpg";
 import hate from "./assets/hate.jpg";
 import dna from "./assets/dna.png";
-import { Analytics } from "@vercel/analytics/react"
 
 const sleep = (time: number) => {
   return new Promise((res) => {
@@ -492,7 +491,6 @@ function App() {
             */}
         </div>
       </div>
-      <Analytics />
     </div>
   )
 }
